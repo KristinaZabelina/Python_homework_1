9f1add623be853b6fb58465c33df5a48d7e39e9e
Kristina <zkvzkvzkv@yandex.ru>
2023-02-27 21:48:16 +0300

    добавлено решение задачи 1

--- hw_1_task_1.py
+++ hw_1_task_1.py
@@ -1,2 +1,9 @@
 # Задача 2: Найдите сумму цифр трехзначного числа.
 
+number = int(input('Введите трехзначное число: '))
+sum = 0
+while number > 0:
+    m = number % 10
+    sum = sum + m
+    number = number//10
+print(sum)
